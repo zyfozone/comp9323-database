85057e2161aebcea0a6ca9a745d5e04d75466c38
bot-Xiaoshell <53509154+bot-Xiaoshell@users.noreply.github.com>
2022-07-10 17:09:40 +1000

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,9 +4,3 @@ DB_PASSWORD = 'YOUR DATABASE PASSWORD'
 DB_NAME = 'YOUR DATABASE NAME'
 DB_URL = 'localhost'
 DB_PORT = 3306
-
-# SMTP email
-SMTP_SERVER = 'smtp.gmail.com'
-SMTP_PORT = 465
-EMAIL_ADDRESS = 'WellbeingBridge@xxxxx.com'
-EMAIL_PASSWORD = 'xxxxxxx'
