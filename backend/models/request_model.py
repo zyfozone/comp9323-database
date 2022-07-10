5d6cca7a2d02547941328edc2ea79701045dd015
zyfozone <85334354+zyfozone@users.noreply.github.com>
2022-07-10 17:19:08 +0800

    Add files via upload

--- backend/models/request_model.py
+++ backend/models/request_model.py
@@ -6,15 +6,81 @@ from flask_app import api
 individual_model = api.model("individual", {
     "IndividualName": fields.String,
     "Password": fields.String,
-    "Occupation": fields.String
+    "Preference": fields.String,
+    "Occupation": fields.String,
 })
 
 organization_model = api.model("organization", {
     "OrganizationName": fields.String,
-    "Password": fields.String
+    # "email": fields.String,
+    "password": fields.String,
+    # "preference": fields.String,
 })
 
 login_model = api.model("login", {
     "Name": fields.String,
-    "Password": fields.String
+    "password": fields.String
+})
+
+user_model = api.model("user", {
+    "UserId": fields.Integer,
+    "Nickname": fields.String,
+    "Email": fields.String,
+    "Password": fields.String,
+    "FavouriteId": fields.String
+})
+
+location_model = api.model("location", {
+    "postcode": fields.String,
+    "address": fields.String,
+    "lat": fields.String,
+    "lng": fields.String
+})
+
+event_model = api.model("event", {
+    "eventName": fields.String,
+    "thumbnail": fields.String,
+    "format": fields.String,
+    "category": fields.String,
+    "location": fields.Nested(location_model),
+    "startdate": fields.String,
+    "enddate": fields.String,
+    "time": fields.String,
+    "introduction": fields.String,
+    "details": fields.String
+})
+
+org_model = api.model("org", {
+    "organizationName": fields.String,
+    "organizationType": fields.String,
+    "logo": fields.String,
+    "contact": fields.String,
+    "introduction": fields.String,
+    "details": fields.String,
+    "video": fields.String,
+    "serviceList": fields.String,
+    "websiteLink": fields.String
+})
+
+user_update_model = api.model("user", {
+    "Nickname": fields.String,
+    "Password": fields.String,
+})
+
+org_profile_model = api.model("profile", {
+    "organizationName": fields.String,
+    "password": fields.String
+})
+
+comment_model = api.model("comment", {
+    "comment": fields.String
+})
+
+reply_model = api.model("reply", {
+    "answer": fields.String
+})
+
+review_model = api.model("review", {
+    "review": fields.String,
+    "rating": fields.Integer
 })
