fd2cf1a3aa52960825420f36e9928430540dc8c0
bot-Xiaoshell <53509154+bot-Xiaoshell@users.noreply.github.com>
2022-07-12 14:11:03 +1000

    Update tool.py

--- backend/tool.py
+++ backend/tool.py
@@ -17,3 +17,30 @@ def sql_command(command):
     db.close()
     return result
 
+def sql_result_with_decription(command):
+    db = pymysql.connect(
+        host=DB_URL,
+        port=DB_PORT,
+        user=DB_ACCOUNT,
+        password=DB_PASSWORD,
+        database=DB_NAME,
+        charset='utf8'
+    )
+    c = db.cursor()
+    c.execute(command)
+    sql_result = c.fetchall()
+    cols = c.description
+    col=[]
+    for v in cols:
+        col.append(v[0])
+    db.commit()
+    db.close()
+
+    result = result_package(col,sql_result[0])
+    return result
+
+def result_package(keys,values):
+    result = {}
+    for m,n in zip(keys,values):
+        result[m] = n
+    return result
